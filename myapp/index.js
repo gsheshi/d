b41c878a7fea47122889c0a24f6b342f25fa9b6d
sheshiKumar <gundlasheshikumar@gmail.com>
2025-01-10 08:49:13 +0000

    update book API

--- myapp/index.js
+++ myapp/index.js
@@ -59,6 +59,80 @@ app.get('/books/:bookId/', async (request, response) => {
 
 //Add Book API
 app.post('/books', async (request, response) => {
-  const bookObj = request.body
-  // const {}
+  const bookDetails = request.body
+  // console.log(bookDetails)
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+         '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        "${description}",
+         ${pages},
+        '${dateOfPublication}',
+        "${editionLanguage}",
+         ${price},
+        '${onlineStores}'
+      );`
+  const dbResponse = await db.run(addBookQuery)
+  const bookId = dbResponse.lastID
+  response.send({bookId})
+
+  //update book API
+  app.put("/books/:bookId/", async (request, response) => {
+    const {bookId} = request.params
+    const bookDetails = request.body
+    const {
+      title,
+      authorId,
+      rating,
+      ratingCount,
+      reviewCount,
+      description,
+      pages,
+      dateOfPublication,
+      editionLanguage,
+      price,
+      onlineStores,
+    } = bookDetails
+
+    const updateBookQuery = `
+      UPDATE
+        book
+      SET
+        title='${title}',
+        author_id=${authorId},
+        rating=${rating},
+        rating_count=${ratingCount},
+        review_count=${reviewCount},
+        description='${description}',
+        pages=${pages},
+        date_of_publication='${dateOfPublication}',
+        edition_language='${editionLanguage}',
+        price= ${price},
+        online_stores='${onlineStores}'
+      WHERE
+        book_id = ${bookId};`
+
+    await db.run(updateBookQuery)
+
+    response.send('book updated')
+  })
 })
